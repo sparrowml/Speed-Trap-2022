Fixed the broadcasting issue

speed_trapv3/keypoints/dataset.py
@@ -104,9 +104,6 @@ def get_sample_dicts(holdout: Optional[Holdout] = None) -> list[dict[str, Any]]:
             for p in Config.annotations_directory.glob("*.json")
         ]
     )
-    import os
-
-    print("Dr.Dre says ", os.listdir(str(Config.annotations_directory)))
     samples = []
 
     for slug in slugs:
@@ -142,6 +139,7 @@ class SegmentationDataset(torch.utils.data.Dataset):
         """Get the tensor dict for a sample."""
         sample = self.samples[idx]
         resize_height, resize_width = Config.image_resize
+        print("Frodo says", sample["keypoints"].shape, sample["image_path"])
         keypoints = sample["keypoints"] * np.array([resize_width, resize_height])
         heatmaps = []
         for x, y in keypoints:

speed_trapv3/keypoints/model.py
@@ -11,7 +11,7 @@ class SegmentationModel(torch.nn.Module):
 
     def __init__(self) -> None:
         super().__init__()
-        self.fcn = fcn_resnet50(num_classes=8, pretrained_backbone=True, aux_loss=False)
+        self.fcn = fcn_resnet50(num_classes=2, pretrained_backbone=True, aux_loss=False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Run a forward pass with the model."""