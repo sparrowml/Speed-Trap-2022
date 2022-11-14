81f7507e2e0df6d2b879b5fdb80579e2a42fb3dc
ktjayamanna <kaveen.jayamanna@gmail.com>
2022-11-14 23:05:49 +0000

    EoD: Started evaluating the speed; still not working.

---
+++ speed_trapv3/utilityPyScripts/video_to_imgs.py
@@ -0,0 +1,6 @@
+from speed_trapv3.sampling import video_to_frames
+
+video_to_frames(
+    "/code/data/datasets/source_videos/25_resampled_vid.mp4",
+    "/code/data/datasets/common_hall/demo_imgs",
+)
